fbaeea838301bdff1e1dd065d80f86f7bd4bb2b8
Elijah Chen <elijahbecerrakc@gmail.com>
2023-11-27 18:02:24 -0800

    completed Scroll component

---
+++ src/Scroll.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Scroll = (props) => {
+  return (
+    <div
+      style={{ overflow: "scroll", height: "760px", border: "2px solid black" }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Scroll;
